c9b4a57067bf39013deca35891c912962694ffd9
Dmitriy <dm.elackov@gmail.com>
2024-04-20 15:49:52 +0300

    CRUD db

--- api_server/database/crud.py
+++ api_server/database/crud.py
@@ -17,3 +17,35 @@ async def get_log_by_neuro_ray_id(session: AsyncSession, neuro_ray_id: str) -> H
     stmt = select(History).where(History.neuro_ray_id == neuro_ray_id)
     res = await session.scalar(stmt)
     return res
+
+
+async def create_metro_station(session: AsyncSession, name: str, line_id: str):
+    station = MetroStation(name=name, line_id=line_id)
+    session.add_all([station])
+    await session.commit()
+    return station
+
+
+async def create_metro_line(session: AsyncSession, name: str, line_number: int):
+    station = MetroLine(name=name, line_number=line_number)
+    session.add_all([station])
+    await session.commit()
+    return station
+
+
+async def get_metro_line_by_number(session: AsyncSession, line_number: int) -> MetroLine | None:
+    stmt = select(MetroLine).where(MetroLine.line_number == line_number)
+    res = await session.scalar(stmt)
+    return res
+
+
+async def get_metro_line_by_id(session: AsyncSession, id: int) -> MetroLine | None:
+    stmt = select(MetroLine).where(MetroLine.id == id)
+    res = await session.scalar(stmt)
+    return res
+
+
+async def get_metro_station_by_id(session: AsyncSession, id: int) -> MetroStation | None:
+    stmt = select(MetroStation).where(MetroStation.id == id)
+    res = await session.scalar(stmt)
+    return res
