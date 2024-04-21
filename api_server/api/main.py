fd412b4a4d3e90ee34f4992d158d4a2f8c278d3d
Dmitriy <dm.elackov@gmail.com>
2024-04-21 12:26:12 +0300

    fix

--- api_server/api/main.py
+++ api_server/api/main.py
@@ -117,7 +117,7 @@ async def neuro_hook_preprocess(req: NeuroAnswer, background_tasks: BackgroundTa
                 if fr is not None:
                     traffic = fr.count
             msg = f"There were {traffic} people at {i['station_name']} station on {i['datetime'].strftime('%d.%m.%y')}"
-            line = await crud.get_metro_line_by_id(j.line_id)
+            line = await crud.get_metro_line_by_id(session, j.line_id)
             if line is not None:
                 name = line.name
                 msg += f" on {name} line"
