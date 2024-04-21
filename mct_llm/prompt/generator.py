038145de3c1b0c9a23f4b6a9bb377e3929e6a287
Ksenob1te <beta.ksenobite@mail.ru>
2024-04-21 09:30:01 +0300

    CREATE: prompt generator

--- mct_llm/prompt/generator.py
+++ mct_llm/prompt/generator.py
@@ -5,15 +5,29 @@ import asyncio
 
 
 async def main():
-    print("{", end="")
+    # print("[", end="")
+    # async with async_session_maker() as session:
+    #     stmt = select(MetroStation, MetroLine.line_number, MetroLine.name).join(MetroLine, MetroLine.id == MetroStation.line_id)
+    #     res = await session.execute(stmt)
+    #     for x in res:
+    #         print(
+    #             f"{'('}{x[0].name}, {x[2]}, {x[1]}{')'},",
+    #             end=" ")
+    # print("]", end="")
+    station_list = set()
+    line_list = set()
     async with async_session_maker() as session:
-        stmt = select(MetroStation, MetroLine.line_number, MetroLine.name).join(MetroLine, MetroLine.id == MetroStation.line_id)
+        stmt = select(MetroStation)
         res = await session.execute(stmt)
         for x in res:
-            print(
-                f"{'{'}station_name: {x[0].name}, line_name: {x[2]}, line_number:{x[1]}{'}'},",
-                end=" ")
-    print("}", end="")
+            station_list.add(x[0].name)
+
+        stmt = select(MetroLine)
+        res = await session.execute(stmt)
+        for x in res:
+            line_list.add((x[0].name, x[0].line_number))
+    print(station_list)
+    print(line_list)
 
         # x = next(res)
         # while x:
